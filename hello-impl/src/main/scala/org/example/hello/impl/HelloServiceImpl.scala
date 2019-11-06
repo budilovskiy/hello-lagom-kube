0b75bf565aea8d6889ad09df27274f4e01cfcb07
Maksim Budilovskiy <maksim.budilovskiy@gmail.com>
2019-11-06 15:15:59 +0200

    Initial commit

---
+++ hello-impl/src/main/scala/org/example/hello/impl/HelloServiceImpl.scala
@@ -0,0 +1,54 @@
+package org.example.hello.impl
+
+import org.example.hello.api
+import org.example.hello.api.HelloService
+import com.lightbend.lagom.scaladsl.api.ServiceCall
+import com.lightbend.lagom.scaladsl.api.broker.Topic
+import com.lightbend.lagom.scaladsl.broker.TopicProducer
+import com.lightbend.lagom.scaladsl.persistence.{EventStreamElement, PersistentEntityRegistry}
+
+import scala.concurrent.ExecutionContext
+
+/**
+  * Implementation of the HelloService.
+  */
+class HelloServiceImpl(persistentEntityRegistry: PersistentEntityRegistry)(implicit ec: ExecutionContext) extends HelloService {
+
+  override def hello(id: String) = ServiceCall { _ =>
+    // Look up the hello entity for the given ID.
+    val ref = persistentEntityRegistry.refFor[HelloEntity](id)
+
+    // Ask the entity the Hello command.
+    ref.ask(Hello(id))
+  }
+
+  override def internalHello(id: String) = ServiceCall { _ =>
+    // Look up the hello entity for the given ID.
+    val ref = persistentEntityRegistry.refFor[HelloEntity](id)
+
+    // Ask the entity the Hello command.
+    ref.ask(Hello(id)).map(resp => s"$resp (internal)")
+  }
+
+  override def useGreeting(id: String) = ServiceCall { request =>
+    // Look up the hello entity for the given ID.
+    val ref = persistentEntityRegistry.refFor[HelloEntity](id)
+
+    // Tell the entity to use the greeting message specified.
+    ref.ask(UseGreetingMessage(request.message))
+  }
+
+
+  override def greetingsTopic(): Topic[api.GreetingMessageChanged] =
+    TopicProducer.singleStreamWithOffset {
+      fromOffset =>
+        persistentEntityRegistry.eventStream(HelloEvent.Tag, fromOffset)
+          .map(ev => (convertEvent(ev), ev.offset))
+    }
+
+  private def convertEvent(helloEvent: EventStreamElement[HelloEvent]): api.GreetingMessageChanged = {
+    helloEvent.event match {
+      case GreetingMessageChanged(msg) => api.GreetingMessageChanged(helloEvent.entityId, msg)
+    }
+  }
+}
